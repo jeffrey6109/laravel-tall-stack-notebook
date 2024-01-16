814211210728253255823459ea2b0e7068189900
Jeffrey_Kwan <striker.jk95@gmail.com>
2024-01-16 17:32:21 +0800

    Second commit

---
+++ app/Models/Note.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'send_date',
+        'is_published',
+        'heart_count',
+        'recipient',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function publishedNotes(User $user)
+    {
+        return $this->where('user_id', $user->id)
+            ->where('is_published', true)
+            ->get();
+    }
+}
